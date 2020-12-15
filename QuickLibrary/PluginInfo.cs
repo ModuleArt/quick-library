c4425f9b5266a5ebaa39bbbb573ebed70c67eaa1
Eugene Volynko <volynko.ua@gmail.com>
2020-12-15 20:26:01 +0200

    Built-in plugin manager

---
+++ QuickLibrary/PluginInfo.cs
@@ -0,0 +1,100 @@
+﻿using Utf8Json;
+
+namespace QuickLibrary
+{
+	public class PluginInfo
+	{
+		#region STRUCTS
+
+		public struct Author
+		{
+			public string name;
+			public string link;
+		}
+
+		public struct Target
+		{
+			public string name;
+			public string minVersion;
+			public string maxVersion;
+		}
+
+		public struct Hotkey
+		{
+			public bool ctrl;
+			public bool shift;
+			public int key;
+		}
+
+		public struct MultilangString
+		{
+			public string en;
+			public string ru;
+			public string es;
+
+			public string Get(string langCode)
+			{
+				string value;
+				switch (langCode)
+				{
+					case "ru":
+						value = ru;
+						break;
+					case "es":
+						value = es;
+						break;
+					default:
+						value = en;
+						break;
+				}
+				return value;
+			}
+		}
+
+		public struct Function
+		{
+			public string name;
+			public MultilangString title;
+			public Hotkey hotkey;
+			public bool configurable; // for pluginType = effect
+			public bool inputRequired;
+			public bool source;
+		}
+
+		#endregion
+
+		#region VARIABLES
+
+		public string name;
+		public string version;
+		public string title;
+		public MultilangString description;
+		public string link;
+		public Author[] authors;
+		public int apiVer; // 2
+		public string pluginType; // tool, effect
+		public string inputType; // bitmap
+		public string dllType; // csharp, cpp
+		public Function[] functions;
+
+		#endregion
+
+		#region STATIC METHODS
+
+		public static PluginInfo FromJson(string json)
+		{
+			return JsonSerializer.Deserialize<PluginInfo>(json);
+		}
+
+		#endregion
+
+		#region CONSTRUCTOR
+
+		public PluginInfo()
+		{
+
+		}
+
+		#endregion
+	}
+}
---
+++ QuickLibrary/PluginMan.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace QuickLibrary
+{
+	public class PluginMan
+	{
+		public static string pluginsFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "plugins");
+		public static int apiVer = 2;
+
+		[DllImport("kernel32", SetLastError = true, CharSet = CharSet.Ansi)]
+		public static extern IntPtr LoadLibrary([MarshalAs(UnmanagedType.LPStr)]string lpFileName);
+
+		[DllImport("kernel32", SetLastError = true, EntryPoint = "GetProcAddress")]
+		public static extern IntPtr GetProcAddressOrdinal(IntPtr hModule, string procName);
+
+		public delegate Bitmap RunFunction(
+			Bitmap bmp = null,
+			string path = null,
+			string[] args = null
+		);
+
+		public delegate string[] ConfFunction(
+			Bitmap bmp = null,
+			string path = null,
+			bool darkMode = false,
+			string language = "en",
+			bool alwaysOnTop = false
+		);
+
+		public delegate void PluginOutput(object sender, PluginOutputEventArgs poea);
+
+		public class PluginOutputEventArgs : EventArgs
+		{
+			public object subject { get; set; }
+		}
+
+		public static PluginInfo[] GetPlugins(bool onlyAvailable, string pluginType)
+		{
+			List<PluginInfo> plugins = new List<PluginInfo>();
+			DirectoryInfo di = new DirectoryInfo(pluginsFolder);
+			if (di.Exists)
+			{
+				List<FileInfo> files = new List<FileInfo>();
+				DirectoryInfo[] dirs = di.GetDirectories();
+				for (int i = 0; i < dirs.Length; i++)
+				{
+					files.AddRange(dirs[i].GetFiles());
+					dirs[i] = null;
+				}
+				for (int i = 0; i < files.Count; i++)
+				{
+					if (Path.GetExtension(files[i].Name) == ".json")
+					{
+						PluginInfo pi = PluginInfo.FromJson(File.ReadAllText(Path.Combine(di.FullName, files[i].DirectoryName, files[i].Name)));
+
+						if (onlyAvailable)
+						{
+							if (pi.apiVer == apiVer && pi.pluginType == pluginType)
+							{
+								plugins.Add(pi);
+							}
+						}
+						else
+						{
+							plugins.Add(pi);
+						}
+					}
+					files[i] = null;
+				}
+			}
+			return plugins.ToArray();
+		}
+
+		public static Image GetPluginIcon(string pluginName, string funcName, bool darkMode)
+		{
+			if (darkMode)
+			{
+				string path = Path.Combine(pluginsFolder, pluginName, funcName + ".dark.png");
+				if (File.Exists(path))
+				{
+					return Bitmap.FromFile(path);
+				}
+
+				path = Path.Combine(pluginsFolder, pluginName, funcName + ".png");
+				if (File.Exists(path))
+				{
+					return Bitmap.FromFile(path);
+				}
+
+				return null;
+			}
+			else
+			{
+				string path = Path.Combine(pluginsFolder, pluginName, funcName + ".light.png");
+				if (File.Exists(path))
+				{
+					return Bitmap.FromFile(path);
+				}
+
+				path = Path.Combine(pluginsFolder, pluginName, funcName + ".png");
+				if (File.Exists(path))
+				{
+					return Bitmap.FromFile(path);
+				}
+
+				return null;
+			}
+		}
+	}
+}

41aedd00c7a80570fde4dc2ffba60e3a178d6b62
Adrien GIVRY <contact@adrien-givry.com>
2023-12-14 15:03:08 -0500

    Removed temp files and updated to .net 8.0

---
+++ QuickLibrary/Managers/SerializeMan.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+#pragma warning disable SYSLIB0011 // TODO: Remove this pragma and fix the serializer
+
+namespace QuickLibrary
+{
+	public static class SerializeMan
+	{
+        public static byte[] ObjectToByteArray(Object obj)
+        {
+            if (obj == null)
+                return null;
+
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+            bf.Serialize(ms, obj);
+
+            return ms.ToArray();
+        }
+
+        public static Object ByteArrayToObject(byte[] arrBytes)
+        {
+            MemoryStream memStream = new MemoryStream();
+            BinaryFormatter binForm = new BinaryFormatter();
+            memStream.Write(arrBytes, 0, arrBytes.Length);
+            memStream.Seek(0, SeekOrigin.Begin);
+            Object obj = (Object)binForm.Deserialize(memStream);
+
+            return obj;
+        }
+    }
+}

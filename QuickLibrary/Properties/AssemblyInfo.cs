abbf1785479702973c7c2edb53e5adc10835e17f
Eugene Volynko <volynko.ua@gmail.com>
2023-12-21 21:39:59 +0200

    Add AssemblyInfo for 2.6.0

---
+++ QuickLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("QuickLibrary")]
+[assembly: AssemblyDescription("Windows Forms Control class library for for \"Quick\" apps (.NET Framework)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Module Art")]
+[assembly: AssemblyProduct("QuickLibrary")]
+[assembly: AssemblyCopyright("Copyright © 2023 Module Art")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("90f913b5-1b48-4e7b-83aa-21c0946b4f81")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.6.0")]
+[assembly: AssemblyFileVersion("2.6.0")]
+[assembly: NeutralResourcesLanguage("en")]
